095c6142532ada6857b452cd9b1e55714256ecdd
nitin-codr <nitinporwal99@gmail.com>
2019-12-20 13:23:15 +0530

    used NavLink in place of Link and made post id visible in address bar

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import {Route, Link} from 'react-router-dom';
+import {Route, NavLink} from 'react-router-dom';
 
 import Posts from './Posts/Posts'
 import NewPost from './NewPost/NewPost';
 import './Blog.css';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
     render () {
@@ -12,18 +13,26 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to = '/'>Home</Link></li>
-                            <li><Link to= {{
+                            <li><NavLink 
+                                to = '/' 
+                                exact
+                                activeClassName = 'my-active'
+                                activeStyle = {{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Home</NavLink></li>
+                            <li><NavLink to= {{
                                 pathname:'/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
 
                         </ul>
                     </nav>
                 </header>
                 <Route path = '/' exact component={Posts}/>
                 <Route path = '/new-post' exact component={NewPost}/>
+                <Route path = '/:id' component = {FullPost} />
             </div>
         );
     }
